a0bd4e54c104bf4a094aa4ddab7fe5d614d053f9
ceci138p <ceci138p@stud.kea.dk>
2020-05-03 20:36:59 +0200

    SQL date workaround og CRUD

--- src/main/java/com/example/student_administration/controllers/StudentsController.java
+++ src/main/java/com/example/student_administration/controllers/StudentsController.java
@@ -3,6 +3,7 @@ package com.example.student_administration.controllers;
 import com.example.student_administration.models.Student;
 import com.example.student_administration.repositories.IStudentRepository;
 import com.example.student_administration.repositories.InMemoryStudentRepositoryImpl;
+import com.example.student_administration.repositories.StudentRepositoryImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +14,8 @@ public class StudentsController {
     private IStudentRepository studentRepository;
 
     public StudentsController(){
-        studentRepository = new InMemoryStudentRepositoryImpl();
+        //studentRepository = new InMemoryStudentRepositoryImpl();
+        studentRepository = new StudentRepositoryImpl();
     }
 
     @GetMapping("/students")
